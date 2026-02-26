843dd5b5b0ebe5f74de76758624bddfde1c828a0
nikoloz-code23 <literallynickk@gmail.com>
2026-02-26 20:00:18 +0400

    I updated the Event Parser interface to contain a static variable of the PreviousEventData struct. The purpose is for every event parser to contain the data, which could be helpful if we want to change things up in the future. Along with that, the Push Event Parser got updated with the new logic of counting the amount of commits if they are done back-to-back in a repository. I need to think about how to store the Event Types more efficiently.

--- src/EventHandler/Interface/IEventParser.cs
+++ src/EventHandler/Interface/IEventParser.cs
@@ -1,10 +1,12 @@
 using System.Collections.Generic;
 using System.Text.Json.Nodes;
+using GithubActivity.Structs;
 
 namespace GithubActivity.Interfaces;
 
 public interface IEventParser
 {
     // TODO: Think of efficient logic for shortening duplicates to a count view instead.
+    protected static PreviousEventData prevData;
     public void ParseEvent(JsonNode jsonNode, List<string> result);
 }
--- src/EventHandler/PushEvent.cs
+++ src/EventHandler/PushEvent.cs
@@ -2,17 +2,33 @@ using System;
 using System.Collections.Generic;
 using System.Text.Json.Nodes;
 using GithubActivity.Interfaces;
+using GithubActivity.Structs;
 
 namespace GithubActivity.EventHandler;
 
 public class PushEvent : IEventParser
 {
+    protected static PreviousEventData prevData;
+    private static int amount = 1;
     public void ParseEvent(JsonNode jsonNode, List<string> result)
     {
         string? repositoryName = jsonNode["repo"]?["name"]?.ToString();
+
         if (repositoryName == null)
             throw new Exception("Can't get repository name. JSON is wrong or non-existent. Aborting!");
         
-        result.Add($"- Pushed a commits to {repositoryName}");
+        if (prevData.previousEventType == "PushEvent" && repositoryName == prevData.previousRepository)
+        {
+            amount += 1;
+            result[result.Count-1] = $"- Pushed {amount} commits to {repositoryName}";
+        }
+        else
+        {
+            amount = 1;
+            result.Add($"- Pushed a commit to {repositoryName}");
+        }
+
+        prevData.previousEventType = jsonNode["type"]!.ToString();
+        prevData.previousRepository = repositoryName;
     }
 }
