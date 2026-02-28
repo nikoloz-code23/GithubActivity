d771840c3ac6dd424ce0146efd7ed1bac74a685b
nikoloz-code23 <literallynickk@gmail.com>
2026-02-28 16:14:05 +0400

    Updated GithubEventData. This time around, the data should be clean, easy to use and is utilizing the Payload. This will help us for when we parse the JSON Data.

--- src/Data/GithubData.cs
+++ src/Data/GithubData.cs
@@ -1,26 +1,17 @@
-using System.Text.Json.Nodes;
+using GithubActivity.Structs;
 
 namespace GithubActivity.Data;
 
 public class GithubEventData
 {
-    public JsonNode? CurrentEvent {get; set;}
-    public string? PreviousEventType {get; set;}
-    public string? PreviousRepository {get; set;}
-    
-    public string? GetRepoName()
-    {
-        return CurrentEvent?["repo"]?["name"]?.ToString().ToLower();   
-    }
+    public string EventType { get; set; } = "";
+    public string? RepositoryName { get; set; }
+    public Payload PayloadData { get; set; }
 
-    public string? GetEventType()
+    public GithubEventData(string eventType, string? repoName, Payload payload)
     {
-        return CurrentEvent?["type"]?.ToString().ToLower();
-    }
-
-    public void SetPreviousData()
-    {
-        PreviousEventType = GetEventType();
-        PreviousRepository = GetRepoName();
+        EventType = eventType;
+        RepositoryName = repoName;
+        PayloadData = payload;
     }
 }
